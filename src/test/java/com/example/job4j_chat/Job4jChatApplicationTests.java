db1ce8a183c71299620143212b909137f3da2bf3
xocer <vitamin9292@mail.ru>
2021-11-23 14:04:04 +0300

    initial commit

---
+++ src/test/java/com/example/job4j_chat/Job4jChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job4j_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
